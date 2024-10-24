1c2852ffbd72b891469ec14e281f08b1191276f7
bettercodevivek <viveksingh0520@gmail.com>
2024-10-24 23:41:02 +0530

    Created a Dashboard Component and updated the Page.js file

---
+++ assessment/src/app/Components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Dashboard = () =>{
+    return(
+        <React.Fragment>
+              <h1> I am Dashboard</h1>
+        </React.Fragment>
+    );
+}
+
+
+export default Dashboard;
